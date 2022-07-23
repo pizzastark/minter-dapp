19edbf5c378490b27dbc4c6223eb9845e226ea0e
DegenGame <alidemorg@gmail.com>
2022-07-23 15:26:45 +0200

    updated "constants.js"

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,22 +1,22 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x549d05c2566668276850052078f6Bdf47Aba1Fdc"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
-const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Public Minting Open!!";
-const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
-const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
+const welcome_h1 = "Welcome to testPass NFT Project!";
+const welcome_h2 = "Connect MetaMask to Get Started";
+const welcome_p = 'testPass NFT Project is a test project. Go to <a href="https://mylink.com"  target="_blank" rel="noopener noreferrer">our website</a>';
+const h1_presale_coming_soon = "Whitelist mint starting soon!";
+const h1_presale_mint = "Whitelist Mint is ongoing";
+const h1_public_mint = "Public Mint is ongoing";
+const h2_presale_coming_soon = "Whitelist Mint Countdown";
+const h2_presale_mint = "Public Mint Countdown";
+const p_presale_coming_soon = "Coming soon. Stay tuned for updates!";
+const p_presale_mint_not_whitelisted = "Sorry bruh, you are not whitelisted";
+const p_presale_mint_whitelisted = "LESSGOOO You're on the whitelist!";
+const p_presale_mint_already_minted = "You've already minted greedy boi";
 const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
 const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
+const button_presale_mint_whitelisted = "Whitelist Mint";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
-const mint_failed = "Minting failed. 😢 Please try again.";
+const button_public_mint = "Public Mint";
+const mint_failed = "Mint failed. Please try again.";
